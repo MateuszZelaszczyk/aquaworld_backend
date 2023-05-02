37973b746593402ef2cdb807cc783dc4d6067c0c
Mateusz <63258155+MateuszZelaszczyk@users.noreply.github.com>
2023-05-02 18:48:01 +0200

    up

--- aquaworld_backend/settings.py
+++ aquaworld_backend/settings.py
@@ -91,7 +91,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'aquaworld@gmail.com'
-EMAIL_HOST_PASSWORD = 'nGZQ3FbCVuy8m7c'
+EMAIL_HOST_PASSWORD = 'piyydgdxixpfuxaa'
 EMAIL_USE_TLS = True
 
 AUTH_PASSWORD_VALIDATORS = [
